cc47f3f60d7c0742e359279956e576243a0f6977
erickmob <erickmob@gmail.com>
2019-05-30 15:20:57 -0300

    first

---
+++ whitecode_server/src/main/java/com/erick/whitecode/WhitecodeApplication.java
@@ -0,0 +1,13 @@
+package com.erick.whitecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhitecodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhitecodeApplication.class, args);
+	}
+
+}
